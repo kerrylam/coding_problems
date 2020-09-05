cf786d7517898d5979a205bb9b8500e0e0c76bf7
kqlam21 <kqlam21@gmail.com>
2020-09-04 22:36:24 -0700

    nth fib

--- interview_cake.py
+++ interview_cake.py
@@ -61,27 +61,54 @@ will always come up with the solution.
 
 def get_products_of_all_ints_except_at_index(int_list):
 
-    # Make a list with the products
-    if len(int_list) < 2:
-    	raise IndexError()
+# Make a list with the products
+	if len(int_list) < 2:
+		raise IndexError()
 
-    #this creates an empty list of the exact length we need, saving space
-    products_of_all_ints_except_at_index = [None] * len(int_list)
+#this creates an empty list of the exact length we need, saving space
+	products_of_all_ints_except_at_index = [None] * len(int_list)
 
-    #for each integer, we find the product of all the integers before it
-    #storing the total product so far each time
-    product_so_far = 1
-    for i in range(len(int_list)):
-    	products_of_all_ints_except_at_index[i] = product_so_far
-    	product_so_far *= int_list[i]
+#for each integer, we find the product of all the integers before it
+#storing the total product so far each time
+	product_so_far = 1
+	for i in range(len(int_list)):
+		products_of_all_ints_except_at_index[i] = product_so_far
+		product_so_far *= int_list[i]
 
-    #now for each integer, we find the product of all the integers after it
-    #since each index in products already has the product of all the integers before it
-    #now we're storing the total product of all the other integers
-    product_so_far = 1
-    for i in range(len(int_list) - 1, -1, -1):
-    	products_of_all_ints_except_at_index[i] *= product_so_far
-    	product_so_far *= int_list[i]
+#now for each integer, we find the product of all the integers after it
+#since each index in products already has the product of all the integers before it
+#now we're storing the total product of all the other integers
+	product_so_far = 1
+	for i in range(len(int_list) - 1, -1, -1):
+		products_of_all_ints_except_at_index[i] *= product_so_far
+		product_so_far *= int_list[i]
 
-   	return products_of_all_ints_except_at_index
+	return products_of_all_ints_except_at_index
+
+
+def fib(n):
+	#takes an integer n and returns the nth Fibonacci
+
+	#we want to add to the sequence
+	# we want to add the two previous indexes
+	# so we need to make sure there are 2 before our index
+	# if there isn't 2 before, then it has to be 0 or 1, then we can start after that
+	if n < 0:
+		raise Exception()
+
+	elif n in [0, 1]:
+		return n
+
+	prev_prev = 0
+	prev = 1
+
+	for _ in range(n - 1):
+		current = prev_prev + prev
+		prev_prev = prev
+		prev = current
+
+
+	return current
+
+
 
