41ccb219407863117cda4f1bb210fa26ea9b0b40
kqlam21 <kqlam21@gmail.com>
2020-09-13 00:03:29 -0700

    find largest & second largest node

--- interview_cake.py
+++ interview_cake.py
@@ -215,6 +215,37 @@ def is_binary_search_tree(tree_root):
 	return True
 
 
+def find_largest(root_node):
+	current = root_node
+
+	while current:
+		if not current.right:
+			return current.value
+		current = current.right
+
+
+def find_second_largest(root_node):
+	if (root_node is None or
+			(root_node.left is None and root_node.right is None)):
+		raise ValueError("Tree must have at least 2 nodes")
+
+	current = root_node
+	while current:
+		# Case: current is largest and has a left subtree
+		# 2nd largest is the largest in that subtree
+		if current.left and not current.right:
+			return find_largest(current.left)
+
+		# Case: current is a parent of largest, and largest has on children,
+		# so current is 2nd largest
+		if (current.right and
+				not current.right.left and
+				not current.right.right):
+			return current.right.value
+
+		current = current.right
+
+
 
 	
 
